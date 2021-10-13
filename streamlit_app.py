ea3416e51c94a38ce030075f0ef8c3b2baf2467f
supreethbare <bare.supreeth@gmail.com>
2021-10-13 17:39:24 -0400

    data read from drive

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+from io import StringIO
 
 st.set_page_config(layout="wide")
 st.write("""My First APP""")
 
-data=pd.read_csv(r"C:\Users\supreethbare\Downloads\day_wise.csv")
-# print(len(data))
-st.write("""len(data)""")
-
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis="columns", inplace=True)
-#     data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
-#     return data
+# data=pd.read_csv(r"C:\Users\supreethbare\Downloads\day_wise.csv")
+# # print(len(data))
+url='https://drive.google.com/file/d/1G2AVl-ANLHHkwMqyGkqCwJcFeb_zLlJz/view?usp=sharing'
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df = pd.read_csv(path)
+st.write("""len(df)""")
